1c2d3f0b076c3a474d13930d6d553a8d4f34b2c0
nkpn <illinmikita@gmail.com>
2021-07-30 01:09:40 +0300

    РАБОЧАЯ ВЕРСИЯ

--- src/services/imageAPI.js
+++ src/services/imageAPI.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const URL = 'https://pixabay.com/api/';
 const API_KEY = '21962068-642cbe2e0c24045a0d25727d8';
 
-export const fetchImages = async (searchQuery, page = 1, perPage = 12) => {
+export const fetchImages = async ({ searchQuery, page = 1, perPage = 12 }) => {
   return await axios.get(
     `${URL}?image_type=photo&orientation=horizontal&q=${searchQuery}&per_page=${perPage}&key=${API_KEY}&page=${page}`,
   );
